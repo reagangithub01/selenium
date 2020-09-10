e789a6969992a97995e0db2748f1a814365263c7
RRCWin1064 <RRCWin1064@DESKTOP-4T5QK25>
2020-09-10 03:09:54 -0700

    tried WebDriver Manager but not working as of the moment

--- TestSelenium/src/test/FirstSeleniumTest.java
+++ TestSelenium/src/test/FirstSeleniumTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstSeleniumTest {
 
@@ -9,9 +10,17 @@ public class FirstSeleniumTest {
 		try {
 			
 			System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+			//WebDriverManager.chromedriver().setup(); // with error
 			WebDriver driver = new ChromeDriver();
-			String url = "https://www.selenium.dev/";
+			
+			driver.manage().window().maximize();
+			String url = "https://hago-pilot-model-portal.herokuapp.com/";
 			driver.get(url);
+			System.out.println("Chrome browser running");
+			//5 sec delay
+			Thread.sleep(5000);
+			driver.close();
+			System.out.println("Chrome browser closed");
 			
 		}catch (Exception exp) {
 			System.out.println("Cause is " + exp.getCause());
